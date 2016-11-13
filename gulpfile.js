49c2727ad67af2120240503d4f17d451059b7ed9
Jessica Parsons <verythorough@gmail.com>
2016-11-13 08:12:22 -0800

    App shell precache setup

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// require(d) gulp for compatibility with sublime-gulp.
+var gulp = require('gulp');
+
+gulp.task('generate-service-worker', function(callback) {
+  var path = require('path');
+  var swPrecache = require('sw-precache');
+  var rootDir = 'public';
+
+  swPrecache.write(path.join(rootDir, 'sw.js'), {
+    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,json}'],
+    stripPrefix: rootDir
+  }, callback);
+});
